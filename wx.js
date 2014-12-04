17208175d51723c49d927882863c086d2f437b7e
New Future <new_future@msn.cn>
2014-12-04 20:06:17 +0800

    调试wx.js

--- wx.js
+++ wx.js
@@ -242,12 +242,44 @@ WeixinApi.ready(function(Api) {
         "desc": "更方便的校园打印，南开等你!",
         "title": "云印南开"
     };
-      // 用户点开右上角popup菜单后，点击分享给好友，会执行下面这个代码
-                Api.shareToFriend(wxData);
+
+     // 分享的回调
+                var wxCallbacks = {
+                    // 收藏操作是否触发回调，默认是开启的
+                    favorite : false,
+
+                    // 分享操作开始之前
+                    ready : function() {
+                        // 你可以在这里对分享的数据进行重组
+                       // alert("准备分享");
+                    },
+                    // 分享被用户自动取消
+                    cancel : function(resp) {
+                        // 你可以在你的页面上给用户一个小Tip，为什么要取消呢？
+                       // alert("分享被取消，msg=" + resp.err_msg);
+                    },
+                    // 分享失败了
+                    fail : function(resp) {
+                        // 分享失败了，是不是可以告诉用户：不要紧，可能是网络问题，一会儿再试试？
+                      //  alert("分享失败，msg=" + resp.err_msg);
+                    },
+                    // 分享成功
+                    confirm : function(resp) {
+                        // 分享成功了，我们是不是可以做一些分享统计呢？
+                       // alert("分享成功，msg=" + resp.err_msg);
+                    },
+                    // 整个分享过程结束
+                    all : function(resp,shareTo) {
+                        // 如果你做的是一个鼓励用户进行分享的产品，在这里是不是可以给用户一些反馈了？
+                        //alert("分享" + (shareTo ? "到" + shareTo : "") + "结束，msg=" + resp.err_msg);
+                    }
+                };
+                // 用户点开右上角popup菜单后，点击分享给好友，会执行下面这个代码
+                Api.shareToFriend(wxData, wxCallbacks);
 
                 // 点击分享到朋友圈，会执行下面这个代码
-            Api.shareToTimeline(wxData);
+            Api.shareToTimeline(wxData, wxCallbacks);
 
                 // 点击分享到腾讯微博，会执行下面这个代码
-                Api.shareToWeibo(wxData);
+                Api.shareToWeibo(wxData, wxCallbacks);
 });
