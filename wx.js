407c73ce795fc02cb59d44376c99a20aae487356
New Future <new_future@msn.cn>
2014-12-04 20:01:03 +0800

    分享js

--- wx.js
+++ wx.js
@@ -233,7 +233,7 @@
         }
     };
 })(window);
-
+// WeixinApi.enableDebugMode();
 WeixinApi.ready(function(Api) {
     var wxData = {
         "appId": "",
@@ -241,5 +241,13 @@ WeixinApi.ready(function(Api) {
         "link": "http://print.nkumstc.cn",
         "desc": "更方便的校园打印，南开等你!",
         "title": "云印南开"
-    }
+    };
+      // 用户点开右上角popup菜单后，点击分享给好友，会执行下面这个代码
+                Api.shareToFriend(wxData);
+
+                // 点击分享到朋友圈，会执行下面这个代码
+            Api.shareToTimeline(wxData);
+
+                // 点击分享到腾讯微博，会执行下面这个代码
+                Api.shareToWeibo(wxData);
 });
