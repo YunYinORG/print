36bec2e2f0f6038481d2e5fdedd25cb541371a13
inankai <626237437@qq.com>
2014-12-11 14:01:17 +0800

    修改数据库

--- crebas.sql
+++ crebas.sql
@@ -1,61 +1,66 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2014/12/10 2:11:07                           */
+/* Created on:     2014/12/11 13:32:03                          */
 /*==============================================================*/
 
 
-drop table if exists File;
+drop table if exists file;
 
-drop table if exists Printer;
+drop table if exists printer;
 
-drop table if exists User;
+drop table if exists user;
 
 /*==============================================================*/
-/* Table: File                                                  */
+/* Table: file                                                  */
 /*==============================================================*/
-create table File
+create table file
 (
-   id                   int not null auto_increment,
-   Use_id               int not null,
-   Pri_id               int not null,
+   id                   int not null,
+   use_id               int not null,
+   pri_id               int not null,
    name                 char(32),
    location             char(64),
    time                 datetime,
    printed              bool default 0,
    requirements         char(100),
+   print_amount         int,
+   sides_info           char(10),
+   file_status          char(10),
    primary key (id)
 );
 
 /*==============================================================*/
-/* Table: Printer                                               */
+/* Table: printer                                               */
 /*==============================================================*/
-create table Printer
+create table printer
 (
-   id                   int not null auto_increment,
+   id                   int not null,
    name                 char(20),
    account              char(30),
    password             char(32),
    address              char(30),
+   printer_tel          char(20),
    primary key (id)
 );
 
 /*==============================================================*/
-/* Table: User                                                  */
+/* Table: user                                                  */
 /*==============================================================*/
-create table User
+create table user
 (
-   id                   int not null auto_increment,
+   id                   int not null,
    student_number       char(10),
    password             char(32),
    name                 char(6),
    major                char(20),
    sex                  char(3),
+   user_tel             char(20),
    primary key (id)
 );
 
-alter table File add constraint FK_File_of_Printer foreign key (Pri_id)
-      references Printer (id) on delete restrict on update restrict;
+alter table file add constraint FK_file_of_printer foreign key (pri_id)
+      references printer (id) on delete restrict on update restrict;
 
-alter table File add constraint FK_File_of_User foreign key (Use_id)
-      references User (id) on delete restrict on update restrict;
+alter table file add constraint FK_file_of_user foreign key (use_id)
+      references user (id) on delete restrict on update restrict;
 
