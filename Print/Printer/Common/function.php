b0c15819c31b8b67f1e06a76035f5455700cf308
New Future <new_future@msn.cn>
2014-12-17 22:23:37 +0800

    printer重构

---
+++ Print/Printer/Common/function.php
@@ -0,0 +1,51 @@
+<?php
+
+// ===================================================================
+// | FileName: 		/Print/Printer/Common/function.php
+// ===================================================================
+// | Printer 打印管理端公用函数库
+// ===================================================================
+// +------------------------------------------------------------------
+// | 云印南开
+// +------------------------------------------------------------------
+// | Copyright (c) 2014 云印南开团队 All rights reserved.
+// +------------------------------------------------------------------
+
+/**
+ * Class and Function List:
+ * Function list:
+ * - pri_id()
+ * Classes list:
+ */
+
+/**
+ *pri_id()
+ *获取打印店id，后端验证
+ *如果未登录使用cookie自动登陆并更新session
+ *@param $redirect_url 重定向url,空不跳转
+ *@return int 打印店id
+ */
+function pri_id($redirect_url = null) 
+{
+	$id           = session('pri_id');
+	if ($id) 
+	{
+		return $id;
+	} else
+	{
+		$token = cookie('token');
+		$info  = auth_token($token);
+		if ($info['type'] == C('PRINTER')) 
+		{
+			return $info['id'];
+		}
+	}
+	
+	if ($redirect_url) 
+	{
+		redirect($redirect_url);
+	} else
+	{
+		return 0;
+	}
+}
