0f3fabe2574ed4aee30b0ee6ef8755e1ad2c53a6
wangxiaodiu <niuliang3@gmail.com>
2014-12-24 22:28:30 +0800

    little bug

--- Print/Home/Model/FeedbackModel.class.php
+++ Print/Home/Model/FeedbackModel.class.php
@@ -7,12 +7,15 @@ class FeedbackModel extends Model {
 
     protected $_validate = array(
         array('email','require','请填写邮箱！'),
+        array('email','email','请填写格式正确的邮箱！'),
         array('phone','require','请填写手机号码！'),
         array('message','require','请填写反馈信息！'),
     );
-    
+
+    /*
     protected $_auto =   array(
     array('time','time',1,'function'),
     );
+    */
 }
 ?>
