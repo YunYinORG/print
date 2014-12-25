daf9c58f514346d41df131dee85daa4bcc0ec13c
New Future <new_future@msn.cn>
2014-12-25 10:00:12 +0800

    添加sae配置

---
+++ Common/Conf/config_sae.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+	'FILE_UPLOAD_TYPE' => 'Sae', 
+	'TMPL_PARSE_STRING' => array(
+		'__PUBLIC__' => 'http://nkuprint.sinaapp.com/Public',
+		 '__UPLOAD__' => 'http://nkuprint-uploads.stor.sinaapp.com',
+		 ),
+	);
