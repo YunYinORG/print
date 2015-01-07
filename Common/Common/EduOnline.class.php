e8aa9ee7986d43a91494ddab17952b7ac3b7b46c
New Future <new_future@msn.cn>
2015-01-08 01:41:39 +0800

    价格和说明修改好

--- Common/Common/EduOnline.class.php
+++ Common/Common/EduOnline.class.php
@@ -15,7 +15,7 @@ function getName($stuID,$pwd)
 		$f->setCookies($cookies);
 		$content = $f->fetch('http://222.30.60.9/meol/welcomepage/student/index.jsp');             
         $name = substr($content,(strlen('<li>'."   ") + strpos($content, '<li>')) + 3,(strlen($content) - strpos($content, '</li>')) * (-1));
-   		return $name;
+   		return iconv('GBK','UTF-8//IGNORE',$name);
 	}
 	else
 	{
