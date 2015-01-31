8b3b6d4e354194623460743dc9e462c9142caed2
New Future <new_future@msn.cn>
2015-01-31 18:29:11 +0800

    API更新

--- API/Controller/FileController.class.php
+++ API/Controller/FileController.class.php
@@ -3,7 +3,7 @@
 // ===================================================================
 // | FileName: 		FileController.class.php
 // ===================================================================
-// | Discription：	NotificationController 消息查询接口
+// | Discription：	FileController 文件操作接口
 //		<命名规范：>
 // ===================================================================
 // +------------------------------------------------------------------
@@ -99,97 +99,138 @@ class FileController extends RestController
 			$data['err']       = '认证失败';
 		} else
 		{
-			if ($info['type'] == C('PRINTER')) 
+			
+			//访问类型
+			switch ($info['type']) 
 			{
+			case C('STUDENT'):
+			case C('STUDENT_API'):
+				$where['use_id']       = $info['id'];
+				break;
+
+			case C('PRINTER'):
+			case C('PRINTER_WEB'):
+				$where['pri_id'] = $info['id'];
+				break;
+
+			default:
+				$data['err']      = '位置类型';
+			}
+			
+			$fid  = I('get.id', null, 'intval');
+			
+			if (!$data && $fid) 
+			{
+				$where['id']      = $fid;
+				$File = M('File');
 				
-				//打印店客户端操作
-				$where['pri_id']       = $info['id'];
-				$where['id']       = I('get.id', null, 'intval');
-				$File  = M('File');
-				
+				//操作类型
 				switch ($this->_method) 
 				{
 				case 'get':
-					
-					//获取文件信息
-					$data  = $File->where($where)->cache(false)->find();
+					$where['status']      = array('gt', '0');
+					$data = $File->where($where)->find();
 					break;
 
 				case 'put':
 				case 'post':
-					
-					//修改文件状态
-					//对于不支持put的操作暂用post代替
-					$where['status']             = array('between', '1,4');
-					$file        = $File->where($where)->field('status,id')->cache(false)->find();
-					if (!$file) 
+					if (!isset($where['pri_id'])) 
 					{
-						$data['err']             = '文件已删除或者已付款！';
+						$data['err']             = '只允许打印店进行此操作';
 					} else
 					{
-						$status      = I('status');
-						switch ($status) 
+						$where['status']             = array('between', '1,4');
+						$file        = $File->where($where)->field('status')->cache(false)->find();
+						if (!$file) 
 						{
-						case 'upload':
-						case 1:
-							$status_code = 1;
-							break;
-
-						case 'download':
-						case 2:
-							$status_code = 2;
-							break;
-
-						case 'printing':
-						case 3:
-							$status_code = 3;
-							break;
-
-						case 'printed':
-						case 4:
-							$status_code = 4;
-							break;
-
-						case 'payed':
-						case 5:
-							$status_code = 5;
-							break;
-
-						default:
-							
-							$data['err'] = '非法状态！';
-							break;
-						}
-						if (!isset($data)) 
+							$data['err']             = '文件已删除或者已付款！';
+						} else
 						{
+							$status      = I('status');
+							switch ($status) 
+							{
+							case 'upload':
+							case 1:
+								$status_code = 1;
+								break;
+
+							case 'download':
+							case 2:
+								$status_code = 2;
+								break;
+
+							case 'printing':
+							case 3:
+								$status_code = 3;
+								break;
+
+							case 'printed':
+							case 4:
+								$status_code = 4;
+								break;
+
+							case 'payed':
+							case 5:
+								$status_code = 5;
+								break;
+
+							default:
+								$data['err'] = '非法状态！';
+								break;
+							}
 							if ($status_code <= $file['status']) 
 							{
 								$data['err'] = '不允许逆向设置！';
-							} elseif ($File->where('id="%d"', $file['id'])->cache(false)->setField('status', $status_code)) 
+							} elseif ($File->where('id="%d"', $fid)->setField('status', $status_code)) 
 							{
 								$data['msg'] = '修改完成！';
 								
 								//删除缓存
-								S(cache_name('printer', $info['id']), null);
-								S(cache_name('user', $file['use_id']), null);
+								// S(cache_name('printer', $info['id']), null);
+								// S(cache_name('user', $file['use_id']), null);
+								
+								
 							} else
 							{
-								$data['err'] = '修改失败';
+								$data['err'] = '状态设置失败';
 							}
 						}
 					}
 					break;
 
-				default:
-					
-					$data['err'] = '不支持操作！';
+				case 'delete':
+					if (!isset($where['use_id'])) 
+					{
+						$data['err']      = '只允许上传用户删除';
+					} else
+					{
+						$where['status']      = array(array('eq', 1), array('eq', 5), 'or');
+						$file = $File->where($where)->field('url,pri_id')->find();
+						if ($file) 
+						{
+							if (delete_file("./Uploads/" . $file['url'])) 
+							{
+								$save['status']      = 0;
+								$save['url']      = '';
+								if ($File->where('id="%d"', $fid)->save($save)) 
+								{
+									
+									//删除缓存
+									// S(cache_name('printer',$file['pri_id']),null);
+									
+									
+								}
+							} else
+							{
+								$data['err']      = '删除失败！';
+							}
+						} else
+						{
+							$data['err']      = '文件不存在！';
+						}
+					}
 					break;
 				}
-			} else
-			{
-				
-				// $where['use_id']             = $info['id'];
-				$data['err']      = '此接口暂只支持打印店客户端！';
 			}
 		}
 		$type = ($this->_type == 'xml') ? 'xml' : 'json';
