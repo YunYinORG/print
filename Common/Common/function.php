8fd60e6b9156960f539b79df30e1698709533631
605527108 <605527108shif@gmail.com>
2015-02-08 22:19:51 -0500

    add TestController

--- Common/Common/function.php
+++ Common/Common/function.php
@@ -225,3 +225,26 @@ function send_mail($toMail, $content, $mailType)
 function send_sms($toPhone, $content, $smsType) 
 {
 }
+
+
+function qiniu_encode($str) // URLSafeBase64Encode
+{
+    $find = array('+', '/');
+    $replace = array('-', '_');
+    return str_replace($find, $replace, base64_encode($str));
+}
+ 
+ 
+function qiniu_sign($url) 
+{//$info里面的url
+    $setting = C ( 'UPLOAD_SITEIMG_QINIU' );
+    $duetime = NOW_TIME + 86400;//下载凭证有效时间
+    $DownloadUrl = $url . '?e=' . $duetime;
+    $Sign = hash_hmac ( 'sha1', $DownloadUrl, $setting ["driverConfig"] ["secrectKey"], true );
+    $EncodedSign = Qiniu_Encode ( $Sign );
+    $Token = $setting ["driverConfig"] ["accessKey"] . ':' . $EncodedSign;
+    $RealDownloadUrl = $DownloadUrl . '&token=' . $Token;
+    return $RealDownloadUrl;
+}
+
+?>
