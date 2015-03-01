ee7dc9b9f14d3ae6ea7c2cec5b9dc5be27a8055b
New Future <new_future@msn.cn>
2015-03-01 16:31:31 +0800

    Card初版

---
+++ Print/Home/Model/CardlogViewModel.class.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Home\Model;
+use Think\Model\ViewModel;
+class CardlogViewModel extends ViewModel
+{
+	public $viewFields = array(
+		'cardlog'            => array(
+			'id',
+			'find_id',
+			'lost_id',
+			'time',
+			'status',
+		) ,
+		'find'            => array(
+			'_table'=>'user',
+			'name'=>'find_name',
+			'student_number'=>'find_number',
+			'_on'            => 'find.id=cardlog.find_id',
+		) ,
+		'lost'            => array(
+			'_table'=>'user',
+			'name'=>'lost_name',
+			'student_number'=>'lost_number',
+			'_on'            => 'lost.id=cardlog.lost_id',
+		) ,
+	);
+}
