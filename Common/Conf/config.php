b0cfcd6efe60645d030ee87990684f16daed792d
wangxiaodiu <niuliang3@gmail.com>
2015-03-13 21:23:56 +0800

    按照Printer的样式调整后台前端

--- Common/Conf/config.php
+++ Common/Conf/config.php
@@ -28,11 +28,15 @@ return array(
     'MODULE_ALLOW_LIST'    =>    array('Home','Printer','Admin',),
     'DEFAULT_MODULE'           =>    'Home',  // 默认模块
 
-    //数据库配置
-    'DB_TYPE'                    =>    'mysql',     // 数据库类型
-    'DB_PORT'                  =>    '3306',        // 端口
-    'DB_PREFIX'               =>    '',    // 数据库表前缀
-   
+    //数据库配置,测试用
+    'DB_TYPE'               =>  'mysql',     // 数据库类型
+    'DB_HOST'               =>  'localhost', // 服务器地址
+    'DB_NAME'               =>  'print0313',          // 数据库名
+    'DB_USER'               =>  'root',      // 用户名
+    'DB_PWD'                =>  '',          // 密码
+    'DB_PORT'               =>  '3306',        // 端口
+    'DB_PREFIX'             =>  '',    // 数据库表前缀
+       
     //模板渲染转义
     'TMPL_PARSE_STRING' => array(
         '__PUBLIC__'     =>   '/Public',
