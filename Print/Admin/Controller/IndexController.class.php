79e254738f44a3fc201e01e9d9a6566f1bcb0ab0
wangxiaodiu <niuliang3@gmail.com>
2015-03-14 12:38:05 +0800

    将Printer单独作为控制器分离
    1.分离printer
    2,取消cookie
    3,增加简单的manage

--- Print/Admin/Controller/IndexController.class.php
+++ Print/Admin/Controller/IndexController.class.php
@@ -2,7 +2,7 @@
 // ===================================================================
 // | FileName:      /Print/Admin/IndexController.class.php
 // ===================================================================
-// | Discription：   AdminController 后台信息管理控制器
+// | Discription：   IndexController 后台信息管理主页控制器
 //      <命名规范：>
 // ===================================================================
 // +------------------------------------------------------------------
@@ -42,8 +42,8 @@ class IndexController extends Controller {
         if(I('post.account' )==C('ADMIN_ACCOUNT') && I('post.password')==C('ADMIN_PWD'))
         {
             session('admin_id', '1');
-            $token = update_token('1', C('ADMIN'));
-            cookie('token', $token, 60 * 15);//15min
+            // $token = update_token('1', C('ADMIN'));
+            // cookie('token', $token, 60 * 15);//15min
             $this->redirect('index');
         }
         else
@@ -51,48 +51,5 @@ class IndexController extends Controller {
             $this->error("WRONG");
         }
     }
-
-    /**
-     *打印店注册
-     */
-    public function printerRegister() {
-        if (!admin_id()) {
-            $this->redirect('index');
-        } else {
-            $this->display();
-        }
-    }
-
-
-    public function addPrinter() {
-        if (!admin_id()) {
-            $this->redirect('index');
-        }
-
-        if(I('post.password') != I('post.password_repeat')){
-            $this->error('两次输入的密码不一致！');
-        }
-
-        $Printer = D('Printer');
-        
-        $data['account']  = I('post.account');
-        $data['password'] = encode(md5(I('post.password')), I('post.account'));
-        $data['name']     = I('post.name');
-        $data['sch_id']   = I('post.sch_id');
-        $data['address']  = I('post.address');
-        $data['phone']    = I('post.phone');
-        $data['qq']       = I('post.qq');
-        
-        if ($Printer->create($data)) {
-            $result  = $Printer->cache(true)->add();
-            if ($result) {
-                $this->success('新增成功', '/Admin/index');
-            } else {
-                $this->error('数据插入失败' . $Printer->getError());
-            }
-        } else {
-            $this->error('数据创建失败:' . $Printer->getError());
-        }
-    }
-
+    
 }
