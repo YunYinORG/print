ebc56f50596b0b5725e608160a35d4f6e8ac828f
wangxiaodiu <niuliang3@gmail.com>
2015-03-15 17:30:39 +0800

    若sae发送邮件失败，则使用phpmailer

--- Common/Conf/config_sae.php
+++ Common/Conf/config_sae.php
@@ -4,6 +4,7 @@ return array(
 	'VERIFY_NKU' => 'Verify.NankaiEduOnline', 
 	'SHOW_PAGE_TRACE' => 0, 
     'NKU_OPEN' => false,
+    'MAIL_WAY' => 'sae', // or 'phpmailer'
 	'FILE_UPLOAD_TYPE' => 'QINIU',
 	 'TMPL_PARSE_STRING' => array(
 	 	'__PUBLIC__' => '/Public', 
