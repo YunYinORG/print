b994cc39db7e3dd96c3ae55788193c0a2b8cd688
New Future <new_future@msn.cn>
2015-03-24 23:52:26 +0800

    图片大小，资源文件整合压缩

--- assets/js/jekyll-search.js
+++ assets/js/jekyll-search.js
@@ -1,3 +1,9 @@
+function GetURLParameter(key) {
+   var re=new RegExp('(?:\\?|&)'+key+'=(.*?)(?=&|$)','gi');
+   var r=[], m;
+   while ((m=re.exec(document.location.search)) != null) r.push(m[1]);
+   return r;
+}
 (function($) {
     $.fn.jekyllSearch = function(options) {
         var settings = $.extend({
