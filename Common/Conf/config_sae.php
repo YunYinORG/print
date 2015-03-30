12ade81470a62d863a9c5f72f8e6c82a94a9ff15
New-Future <new_future@msn.cn>
2015-03-30 11:11:37 +0800

    open NKU reg

--- Common/Conf/config_sae.php
+++ Common/Conf/config_sae.php
@@ -1,14 +1,14 @@
 <?php
 /*SAE配置*/
 return array(
-	'VERIFY_NKU' => 'Verify.NankaiEduOnline', 
-	'SHOW_PAGE_TRACE' => 0, 
-    'NKU_OPEN' => false,
-    'MAIL_WAY' => 'sae', // or 'phpmailer'
+	'VERIFY_NKU' => 'Verify.NankaiProxy',
+	'SHOW_PAGE_TRACE' => 0,
+	'NKU_OPEN' => 1,
+	'MAIL_WAY' => 'sae', // or 'phpmailer'
 	'FILE_UPLOAD_TYPE' => 'QINIU',
-	 'TMPL_PARSE_STRING' => array(
-	 	'__PUBLIC__' => '/Public', 
-	 	'__CDNLIB__'=>'http://apps.bdimg.com/libs',
-	 	'__UPLOAD__' => 'http://nkuprint-uploads.stor.sinaapp.com',
-	 ),
+	'TMPL_PARSE_STRING' => array(
+		'__PUBLIC__' => '/Public',
+		'__CDNLIB__' => 'http://apps.bdimg.com/libs',
+		'__UPLOAD__' => 'http://nkuprint-uploads.stor.sinaapp.com',
+	),
 );
