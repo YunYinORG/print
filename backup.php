93556f867cad63d88aea0e241c62d08a617577be
New Future <new_future@msn.cn>
2015-05-12 16:05:20 +0800

    数据库备份

---
+++ backup.php
@@ -0,0 +1,39 @@
+<?php
+/*数据库备份*/
+define('DOMAIN', 'DB_BACK');
+//define('', '');
+
+$stor = new SaeStorage(SAE_ACCESSKEY, SAE_SECRETKEY);
+
+/*当天数据备份*/
+$date = date('Y-m-d');
+$filename = $date.'sql.zip';
+if (!$stor->fileExists(DOMAIN, $filename))
+{
+	$dj = new SaeDeferredJob();
+	$taskID = $dj->addTask('export', 'mysql', DOMAIN, $filename, SAE_MYSQL_DB, '', '');
+	if ($taskID === false)
+	{
+		var_dump($dj->errno(), $dj->errmsg());
+	}
+	else
+	{
+		echo $taskID;
+	}
+}
+
+/*旧备份清理*/
+/*保留最近7天和每周1和每月1日的数据备份*/
+$week_ago = date('Y-m-d', strtotime('-1 week'));
+$ninety_days_ago = date('Y-m-d', strtotime('-90 day'));
+if (date('N', time()) != '1' && date('d') != '8' && $stor->fileExists('backup', $week_ago.'.sql.zip'))
+{
+	//if ( today != monday && today != 8.th), delete 7 days ago backup
+	$stor->delete('backup', $week_ago.'.sql.zip');
+}
+if (date('d', strtotime('-90 day')) != '1' && $stor->fileExists('backup', $ninety_days_ago.'.sql.zip'))
+{
+	//if ( $90daysago != 1.st ), delete 3 months ago backup
+	$stor->delete('backup', $ninety_days_ago.'.sql.zip');
+}
+?>
